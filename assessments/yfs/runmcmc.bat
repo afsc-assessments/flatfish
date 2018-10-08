bd1bca63cedbc4e0ea838933fe7f70c30f6edf44
jimianelli <jim.ianelli@noaa.gov>
2018-10-08 17:21:53 +0100

    Added runmceval.bat

--- assessments/yfs/runmcmc.bat
+++ assessments/yfs/runmcmc.bat
@@ -3,7 +3,7 @@ set exec=..\..\src\build\release\fm.exe
 :: if EXIST %exec% (mklink /D  %exec% fm.exe ) ELSE echo "file missing, compile source code in fm\src directory "
 :: %1 %2 %3 %4 %5 %6
 if EXIST %exec% (copy %exec% fm.exe ) ELSE echo "file missing, compile source code in fm\src directory "
-awk '{print $'$1'}' mods.dat >mod.ctl
+awk '{print $'%1'}' mods.dat >mod.ctl
 del fm.std
 fm -nox -iprint 400 -mcmc 1000000 -mcsave 200
 fm -mceval
@@ -14,5 +14,5 @@ copy fm.std arc\mod%1.std
 copy fm.rep arc\mod%1.rep
 copy fm_R.rep arc\mod%1_R.rep
 copy mod.ctl arc\mod%1.ctl
-copy mod.ctl arc\mod%1_evalout.rep
+copy evalout.rep arc\mod%1_evalout.rep
 copy fm.psv arc\mod%1.psv
