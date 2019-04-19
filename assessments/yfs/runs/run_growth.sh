263e96f57b63370c5cd96fa94190305d395bf6dd
jimianelli <jim.ianelli@noaa.gov>
2019-04-19 14:32:14 -0700

    Reorganized run directories for YFS

--- assessments/yfs/runs/run_growth.sh
+++
@@ -1,4 +0,0 @@
-rundt.sh growth 1 
-rundt.sh growth 2 
-rundt.sh growth 3 
-rundt.sh growth 4 
--- assessments/yfs/runs/runeval.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm extra_sd.rep
-cp arc/mod$1.ctl mod.ctl
-cp arc/mod$1.psv fm.psv
-fm -nox -iprint 100 -mceval
-cp evalout.rep arc/mod$1_evalout.rep
