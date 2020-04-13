e541b8073f7194e546e811c9848d170db6bbceff
jimianelli <jim.ianelli@gmail.com>
2020-04-13 08:59:16 -0700

    Some org for NRS

---
+++ assessments/nrs/2018/runit.bat
@@ -0,0 +1,11 @@
+:: Iterate runs to get future catch = to future ABCs
+call run %1
+copy future_ABC.rep future_catch.dat
+call run %1
+copy future_ABC.rep future_catch.dat
+call run %1
+copy future_ABC.rep future_catch.dat
+call run %1
+copy future_ABC.rep future_catch.dat
+call run %1
+copy future_ABC.rep future_catch.dat
