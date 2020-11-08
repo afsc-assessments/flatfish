787856aef1fed75b9fe91cf33119418d2bac203e
careymcg <Carey.McGilliard@gmail.com>
2020-11-07 21:25:32 -0800

    switched to pngs in NRS_20.R

--- assessments/nrs/runs/NRS_20.R
+++ assessments/nrs/runs/NRS_20.R
@@ -30,8 +30,8 @@ nmods    <- length(modlst)
 # To get sex ratio output
 names(modlst) <- mod_names
 names(sex_rat) <- mod_names
-thismod <- 3 # the selected model
-thismodname<-"c2mod4"
+thismod <- 1 # the selected model
+thismodname<-"c1"
 length(modlst)
 
 # sex ratio
@@ -47,7 +47,7 @@ p3 <- sex_rat[[thismod]] %>% filter(!Type %in% c("Survey_est", "Survey_obs", "Fi
    ggplot(aes(y=Ratio,x=Year,color=Type)) + geom_line() + theme_few() + ylim(c(.25,.75))
 # this layes them out stacked (/)
   psex <- p1/p2/p3
-  ggsave(paste0("figs/",thismodname,"_sexratios.pdf"),plot=psex,width=6,height=4.0,units="in")
+  ggsave(paste0("figs/",thismodname,"_sexratios.png"),plot=psex,width=6,height=4.0,units="in")
   psex
   
 # Fishing mortality
@@ -59,23 +59,23 @@ M<-modlst[[thismod]]
             ylab("Age")+ geom_contour(aes(z=F),color="darkgrey",size=.5,alpha=.4) + theme_few() +
             scale_fill_gradient(low = "white", high = "red") + scale_x_continuous(breaks=seq(1975,2020,5)) + 
             scale_y_continuous(breaks=seq(5,20,5)) +facet_grid(sex~.);
-  ggsave(paste0("figs/",thismodname,"_FatAge.pdf"),plot=p1,width=8,height=4.0,units="in")
+  ggsave(paste0("figs/",thismodname,"_FatAge.png"),plot=p1,width=8,height=4.0,units="in")
   
     #mean of Fs -------------------------------------
   p2 <- df.g %>% filter(as.numeric(age)>9) %>% group_by(Year,sex) %>% summarise(Apical_F=max(F),Mean=mean(F))          %>%
              ggplot(aes(x=Year,y=Apical_F,color=sex)) + geom_line(size=1) + theme_few() #+ ggtitle("Age 10-20 Mean F")
   p1/p2
   p3 <-p1/p2
- ggsave(paste0("figs/",thismodname,"_F.pdf"),plot=p3,width=8,height=8,units="in")
+ ggsave(paste0("figs/",thismodname,"_F.png"),plot=p3,width=8,height=8,units="in")
              
 
   #Compare recruitment, ssb, and bts for all the models
   p1 <- plot_rec(modlst,xlim=c(1975.5,2020.5))
-  ggsave(paste0("figs/",thismodname,"_rec.pdf"),plot=p1,width=8,height=4.0,units="in")
+  ggsave(paste0("figs/",thismodname,"_rec.png"),plot=p1,width=8,height=4.0,units="in")
   p1 <- plot_ssb(modlst,xlim=c(1975.5,2020.5),alpha=.1)
-  ggsave(paste0("figs/",thismodname,"_ssb.pdf"),plot=p1,width=8,height=4.5,units="in")
+  ggsave(paste0("figs/",thismodname,"_ssb.png"),plot=p1,width=8,height=4.5,units="in")
   p1<-plot_bts(modlst) + theme_few(base_size=11)
-  ggsave(paste0("figs/",thismodname,"_bts.pdf"),plot=p1,width=8,height=4.0,units="in")
+  ggsave(paste0("figs/",thismodname,"_bts.png"),plot=p1,width=8,height=4.0,units="in")
   
 
 #Plotting just the preferred model (thismod) 
@@ -83,40 +83,40 @@ M<-modlst[[thismod]]
 
   #Time-varying fishery selectivity
   p1 <- plot_sel(modlst[[thismod]],title = NULL, alpha = 0.3,styr= 1975,endyr = 1990,bysex = TRUE,sexoverlay = TRUE); p1
-  ggsave(paste0("figs/",thismodname,"_fsh_sel_early.pdf"),plot=p1,width=4,height=8,units="in")
+  ggsave(paste0("figs/",thismodname,"_fsh_sel_early.png"),plot=p1,width=4,height=8,units="in")
  
   p1 <- plot_sel(modlst[[thismod]],title = NULL, alpha = 0.3,styr= 1991,endyr = 2005,bysex = TRUE,sexoverlay = TRUE); p1
-  ggsave(paste0("figs/",thismodname,"_fsh_sel_mid.pdf"),plot=p1,width=4,height=8,units="in")
+  ggsave(paste0("figs/",thismodname,"_fsh_sel_mid.png"),plot=p1,width=4,height=8,units="in")
  
   p1 <- plot_sel(modlst[[thismod]],title = NULL, alpha = 0.3,styr= 2006,endyr = 2020,bysex = TRUE,sexoverlay = TRUE); p1
-  ggsave(paste0("figs/",thismodname,"_fsh_sel_recent.pdf"),plot=p1,width=4,height=8,units="in")
+  ggsave(paste0("figs/",thismodname,"_fsh_sel_recent.png"),plot=p1,width=4,height=8,units="in")
  
 
   #plot survey selectivity
   p1 <- plot_srv_sel(modlst,themod=thismod, title="Survey selectivity",bysex=TRUE,maxage = 20); p1
-  ggsave(paste0("figs/",thismodname,"_bts_sel.pdf"),plot=p1) #,width=4,height=8,units="in")
+  ggsave(paste0("figs/",thismodname,"_bts_sel.png"),plot=p1) #,width=4,height=8,units="in")
   
   #plot survey age comps 
   # These two work - they make really small plots though - come back to this 
   p1<-plot_age_comps(modlst[thismod],title="Survey age compositions",type="survey",styr=1979,endyr=2000)
-  ggsave(paste0("figs/",thismodname,"_survey_age_comps_1.pdf"),plot=p1,width=11,height=8.5,units="in")
+  ggsave(paste0("figs/",thismodname,"_survey_age_comps_1.png"),plot=p1,width=11,height=8.5,units="in")
 
   p1<-plot_age_comps(modlst[thismod],title="Survey age compositions",type="survey",styr=2001,endyr=2019)
-  ggsave(paste0("figs/",thismodname,"_survey_age_comps_2.pdf"),plot=p1,width=11,height=8.5,units="in")
+  ggsave(paste0("figs/",thismodname,"_survey_age_comps_2.png"),plot=p1,width=11,height=8.5,units="in")
   
   #plot fishery age comps
   p1<-plot_age_comps(modlst[thismod],title="Fishery age compositions",type="fishery",styr = 1975,endyr = 1999)
-  ggsave(paste0("figs/",thismodname,"_fish_age_comps_1.pdf"),plot=p1,width=11,height=8.5,units="in")
+  ggsave(paste0("figs/",thismodname,"_fish_age_comps_1.png"),plot=p1,width=11,height=8.5,units="in")
 
   p1<-plot_age_comps(modlst[thismod],title="Fishery age compositions",type="fishery",styr = 2000,endyr = 2019)
-  ggsave(paste0("figs/",thismodname,"_fish_age_comps_2.pdf"),plot=p1,width=11,height=8.5,units="in")
+  ggsave(paste0("figs/",thismodname,"_fish_age_comps_2.png"),plot=p1,width=11,height=8.5,units="in")
   
  #plot catches
   p1<-plot_catch(modlst,themod = thismod,obspred= TRUE); p1
-  ggsave(paste0("figs/",thismodname,"total_catches_w_pred.pdf"),plot=p1,width=11,height=8.5,units="in")
+  ggsave(paste0("figs/",thismodname,"total_catches_w_pred.png"),plot=p1,width=11,height=8.5,units="in")
 
   p1<-plot_catch(modlst,themod = thismod,obspred= FALSE); p1
-  ggsave(paste0("figs/",thismodname,"total_catches.pdf"),plot=p1,width=11,height=8.5,units="in")
+  ggsave(paste0("figs/",thismodname,"total_catches.png"),plot=p1,width=11,height=8.5,units="in")
   
   thenames<-cbind("Likelihood Component","15.1","18.3","18.3 Downwt")
   like<-cbind("Total",modlst[[1]]$obj_fun,modlst[[2]]$obj_fun,modlst[[3]]$obj_fun)
@@ -173,7 +173,7 @@ SSB<-paste0(1000*tier1.df$SSB[tier1.df$Year==endyr+1],"   ",1000*tier1.df$SSB[ti
 B0<-paste0(1000*modlst[[thismod]]$Bzero,"   ",1000*modlst[[thismod]]$Bzero)
 Bmsy<-paste0(1000*tier1.df$Bmsy[tier1.df$Year==endyr+1],"   ",1000*tier1.df$Bmsy[tier1.df$Year==endyr+2])
 Fofl<-paste0(round(tier1.df$AM_Fmsyr[tier1.df$Year==endyr+1],3),"   ",round(tier1.df$AM_Fmsyr[tier1.df$Year==endyr+2],3))
-maxFabc<-paste0("?   ?")
+maxFabc<-paste0(round(tier1.df$HM_Fmsyr[tier1.df$Year==endyr+1],3),"   ",round(tier1.df$HM_Fmsyr[tier1.df$Year==endyr+2],3))
 Fabc<-paste0(round(tier1.df$HM_Fmsyr[tier1.df$Year==endyr+1],3),"   ",round(tier1.df$HM_Fmsyr[tier1.df$Year==endyr+2],3))
 OFL<-paste0(1000*tier1.df$OFL_AM[tier1.df$Year==endyr+1],"   ",1000*tier1.df$OFL_AM[tier1.df$Year==endyr+2])
 maxABC<-paste0(1000*tier1.df$ABC_HM[tier1.df$Year==endyr+1],"   ",1000*tier1.df$ABC_HM[tier1.df$Year==endyr+2])
@@ -191,3 +191,5 @@ exec.df<-rbind(Mstuff,
                OFL,
                maxABC,
                ABC)
+
+write.table(exec.df,paste0(getwd(),"/tables/",thismodname,"_execsumm.dat"),quote = F)
